ed4f1964daa072851bf637d51490b7359239c1e6
15881074578 <15881074578@163.com>
2017-12-13 16:10:33 +0800

    add CommandController

---
+++ src/main/java/application/controllers/CommandController.java
@@ -0,0 +1,14 @@
+package application.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommandController extends CommandRestInvoker {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    void processResponse(Object object) {
+
+    }
+}
